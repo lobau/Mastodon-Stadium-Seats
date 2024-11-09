e1463c09851aa6c890e2d875c45458070b138c0b
Laurent Baumann <git@lobau.io>
2024-11-09 00:50:09 -0500

    testing layout

--- mss.js
+++ mss.js
@@ -1,14 +1,19 @@
 const fetchDataAndDisplay = () => {
   // Clear previous data
-  document.getElementById('posts').innerHTML = '';
+  document.getElementById("posts").innerHTML = "";
 
-  const querykeys = ['rss', 'foss','opensource','mastodon',]; // replace these hashtag searches with your own
-  
-  const domains = ['mastodon.social', 'mstdn.social','mastodon.online','mastodon.world','techhub.social'];
-  // Replace these instances with your own
-  
-  const corsProxyUrl = 'https://corsproxy.io/?';
-  
+  // The querykeys array below holds the hashtags you'lll be monitoring. You can have an unlimited number but the more you have the longer the feed will take to generate and it might change too fast.
+  // Make sure to put single quotes around any new hashtags you add, and separate them with commas.
+  const querykeys = ["mostodon", "lichensubscribe"];
+  // The domains array holds the Mastodon instances you'll be monitoring. I find these work well but you can change them if you wish.
+  const domains = [
+    "mastodon.social",
+    "mstdn.social",
+    "mastodon.world",
+    "techhub.social",
+  ];
+  const corsProxyUrl = "https://corsproxy.io/?";
+
   // Create an array to hold all the results
   let allPosts = [];
 
@@ -21,12 +26,13 @@ const fetchDataAndDisplay = () => {
   // Iterate through each domain
   domains.forEach((domain) => {
     querykeys.forEach((querykey) => {
-      const url = 'https://' + domain + '/api/v1/timelines/tag/' + querykey + '?limit=40';
+      const url =
+        "https://" + domain + "/api/v1/timelines/tag/" + querykey + "?limit=40";
 
       let fetchPromise = fetch(corsProxyUrl + url)
         .then((response) => {
           if (!response.ok) {
-            throw new Error('HTTP error ' + response.status);
+            throw new Error("HTTP error " + response.status);
           }
           return response.json();
         })
@@ -40,7 +46,7 @@ const fetchDataAndDisplay = () => {
           });
         })
         .catch(function () {
-          console.log('An error occurred while fetching the data.');
+          console.log("An error occurred while fetching the data.");
         });
 
       fetchPromises.push(fetchPromise);
@@ -52,46 +58,39 @@ const fetchDataAndDisplay = () => {
     let top15Posts = allPosts.slice(0, 15);
 
     // Inserting data into HTML
-    const postsContainer = document.getElementById('posts');
-
-    // Create two columns
-    const column1Div = document.createElement('div');
-    column1Div.className = 'column';
-
-    const column2Div = document.createElement('div');
-    column2Div.className = 'column';
+    const postsContainer = document.getElementById("posts");
 
     // Iterate through the top 15 posts, and append them to the appropriate column
     top15Posts.forEach((post, index) => {
-      const postDiv = document.createElement('div');
-      postDiv.className = 'post';
-       if (index % 2 === 0) {
-    postDiv.classList.add('shaded');  
-  }
+      if (post.media_attachments.length > 0) {
+        if (post.media_attachments[0].type == "image") {
+          // console.log(post);
 
-      // Create a temporary container to hold the HTML content
-      let tempDiv = document.createElement('div');
-      tempDiv.innerHTML = post.content;
- 
-      // Append the manipulated content
-      postDiv.innerHTML = tempDiv.innerHTML;
+          const postImg = post.media_attachments[0];
+          const imgUrl = postImg.url;
+          const imgAlt = postImg.alt;
 
-      // Depending on the index, append the post to the appropriate column
-      if (index < 7) {
-        column1Div.appendChild(postDiv);
-      } else if (index < 14) {
-        column2Div.appendChild(postDiv);
+          const postDiv = document.createElement("a");
+          postDiv.className = "post";
+          postDiv.tabIndex = 1;
+          // postDiv.id = index;
+          // postDiv.href = `#${index}`;
+          postDiv.innerHTML = `
+                                    <img src='${imgUrl}' alt='${imgAlt}' />
+                                    <div class='post-content'>
+                                      ${post.content}
+                                    </div>
+                                `;
+
+          postsContainer.appendChild(postDiv);
+        }
       }
     });
-
-    // Append the two columns to the posts container
-    postsContainer.appendChild(column1Div);
-    postsContainer.appendChild(column2Div);
   });
 };
 
 // Call the function immediately to fetch and display data
 fetchDataAndDisplay();
 
-// Schedule the function to run every 90 seconds, you can of course adjust this
+// Schedule the function to run every 90 seconds (expressed in milliseconds.) You can change this if you wish.
 setInterval(fetchDataAndDisplay, 90000);
